d7a849ae191c7c9814915a6ad881b0f0aab16eb0
yuntaewon <sun10000ss@gmail.com>
2026-03-30 14:44:38 +0900

    2차원배열

---
+++ array/2array-2.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+int main(void)
+{
+    int i;
+    char menu[5][10] =/*５행　１０열（０～４행　０～９열）*/
+    {
+        "init",//1형씩 초기값 설정
+        "open",
+        "close",
+        "read",
+        "write"
+    };
+
+    for (i = 0; i < 5; i++)
+        printf("%d 번째 메뉴: %s \n", i, menu[i]); //menu[i]는 행을 의미
+
+    return 0;
+}
