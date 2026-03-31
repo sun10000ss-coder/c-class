395912e126e3c303b5154cc352745a52066d4d2a
yuntaewon <sun10000ss@gmail.com>
2026-03-31 12:39:53 +0900

    0331

---
+++ array/sprcmp-1.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+int main(void) 
+{
+	int res;
+	char a[30];
+	char b[30];
+	printf("첫번째 단어를 입력하시요");
+	scanf("%s", a);
+	printf("두번쨰 단어를 입력하시요:");
+	scanf("%s", b);
+	res =strcmp(a, b);//문자열비교
+	if (res < 0)//음수이면 문자열1일 문자열2보다 사전순으로 앞에 있음
+	
+		printf("%s가 %s보다 앞이다\n", a, b);
+	else if(res == 0)//변환값이 0이면 완전히 같다
+		printf("%s가 %s보다 같습니다\n", a, b);
+	else
+		printf("%s가 %s보다 뒤에있다\n", a, b);
+	//양수이면 분자열1이 문자열2보다 사전순으로 뒤에 있음
+	return 0;
+}
---
+++ array/strcpy-1.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>//#include :전처리기, 입출력 관력 헤더파일 
+#include <string.h>//문자열 함수 사용위한 헤더파일
+int main(void)
+{
+	char a[10]="hello";//h->a[0]
+	printf("%s\n", a);
+	printf("%c\n", a[0]);
+	
+	char b[10];
+	//b = "love";//오류
+	strcpy( b, "love" );//문자열 복사
+	/*b[0] = 'i';
+	b[1] = 'o';
+	b[2] = 'v';
+	b[3] = 'e';*/
+	printf("%s\n", b);
+	//strcpy : 분자배열을 선언이후 뒤에 초기화가 안됨
+	//이후 문자열 복사할때
+	return 0;
+}
