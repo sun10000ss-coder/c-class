5efb400f40cadae228de1eb9ae37fbec4ff7302f
yuntaewon <sun10000ss@gmail.com>
2026-04-02 12:49:42 +0900

    함수

---
+++ function_ex/func-2.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int fun1();
+int fun2();
+int main(void) {
+	printf("메인 시작\n");//1
+	fun1();//2함수호출
+	printf("메인 끝\n");//10
+	return 0;//11
+}
+int fun1() {
+	printf("fun1함수 시작\n");//3
+	fun2();//4
+	printf("fun1함수 끝\n");
+	return 0;//9
+}
+int fun2() {
+	printf("fun2함수 시작\n");//5
+	printf("fun2함수 끝\n");//6
+	return 0;//7
+}
